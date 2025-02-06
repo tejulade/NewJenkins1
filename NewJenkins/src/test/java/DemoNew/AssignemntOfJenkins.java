4f73433267519dfe3eb611656a96a9b9dd4faba8
tejulade <ladehtejaswini@gmail.com>
2025-02-06 20:11:49 +0530

    changes

--- NewJenkins/src/test/java/DemoNew/AssignemntOfJenkins.java
+++ NewJenkins/src/test/java/DemoNew/AssignemntOfJenkins.java
@@ -1,11 +1,14 @@
 package DemoNew;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 public class AssignemntOfJenkins {
 	@Test
 	public void NewJenkin() {
+		WebDriver driver=new ChromeDriver();
 	Reporter.log("AssignemntOfJenkins class is added",true);
 		
 	}
