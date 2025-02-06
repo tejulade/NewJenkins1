694da10501258e721b898f88a6a5825696916024
tejulade <ladehtejaswini@gmail.com>
2025-02-06 19:28:47 +0530

    AssignmentOfJenkins class Added

---
+++ NewJenkins/src/test/java/DemoNew/AssignemntOfJenkins.java
@@ -0,0 +1,13 @@
+package DemoNew;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AssignemntOfJenkins {
+	@Test
+	public void NewJenkin() {
+	Reporter.log("AssignemntOfJenkins class is added",true);
+		
+	}
+
+}
